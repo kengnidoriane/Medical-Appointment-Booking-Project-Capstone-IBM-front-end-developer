0545d33f6b9315ad615e91d463ee0d0856520bbf
kengnidoriane <dorianekengni@gmail.com>
2025-02-20 09:15:04 +0100

    create LandingPage.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing_Page />}/>
+        </Routes>
       </BrowserRouter>
     </div>
   )
