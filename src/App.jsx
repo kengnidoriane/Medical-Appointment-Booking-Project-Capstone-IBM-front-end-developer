98b1a3858597783bbb430bcdab26888e9af7af9d
unknown <dorianekengni@gmail.com>
2025-02-22 11:39:53 +0200

    add code on DoctorCArd component and add more style with it

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Navbar from './Components/Navbar/Navbar';
 // import SignUp from "./Components/Sign_up/Sign_Up";
 // import Login from "./Components/Login/Login";
 // import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
-import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
+// import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
+import DoctorCard from "./Components/DoctoCard/DoctorCard";
 
 function App() {
 
   return (
     <div className='App'>
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path='/' element={<FindDoctorSearch />}/>
+          <Route path='/' element={<DoctorCard name={"linda rodriguez"} speciality={"Dentist"} experience={"9 years experience"} ratings={3}/>}/>
           {/* <Route path='/login' element={<Login />}/>
           <Route path='/signup' element={<SignUp />}/> */}
           {/* <Route path="/instant-consultation" element={<InstantConsultation />}></Route> */}
--- src/Components/DoctoCard/DoctorCard.jsx
+++ src/Components/DoctoCard/DoctorCard.jsx
@@ -1,9 +1,31 @@
-// import React from 'react'
+import './DoctorCard.css';
 
-function DoctorCard() {
+
+const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
+
   return (
-    <div>DoctorCard</div>
-  )
-}
+    <div className="doctor-card-container">
+      <div className="doctor-card-details-container">
+        <div className="doctor-card-profile-image-container">
+        <i style={{ color: '#000000', fontSize: '7rem' }} className="fa fa-user-md"></i>
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/> </svg> */}
+        {/* {<img src={profilePic} alt="" />|| } */}
+        </div>
+        <div className="doctor-card-details">
+          <div className="doctor-card-detail-name">Dr. {name} </div>
+          <div className="doctor-card-detail-speciality">{speciality}</div>
+          <div className="doctor-card-detail-experience">{experience} years experience</div>
+          <div className="doctor-card-detail-consultationfees">Ratings: {ratings}</div>
+        </div>
+        <div>
+                <button className='book-appointment-btn'>
+                    <div>Book Appointment</div>
+                    <div>No Booking Fee</div>
+                </button>
+              </div>
+      </div>
+    </div>
+  );
+};
 
-export default DoctorCard
+export default DoctorCard;
