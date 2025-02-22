dc07dc0325f02318c2ef43dcdc6bcc7b3e0808a4
unknown <dorianekengni@gmail.com>
2025-02-22 09:18:10 +0200

    add FindSearchDoctor folder with his file

---
+++ src/Components/FindDoctorSearch/FindDoctorSearch.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+import './FindDoctorSearch.css'
+
+function FindDoctorSearch() {
+  return (
+    <div>FindDoctorSearch</div>
+  )
+}
+
+export default FindDoctorSearch
